a3533894693ad0fbd8d88a10c97ff81967c1a1ca
SUL1SS <617746883@qq.com>
2019-08-19 10:06:16 +0800

    readme

--- application/common/model/Message.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace app\common\model;
-use think\Db;
-use think\Request;
-class Message extends \think\Model
-{
-	
-}
--- application/common/model/Plugin.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace app\common\model;
-use think\Db;
-use think\Request;
-use think\Cache;
-class Plugin extends \think\Model
-{
-	
-}
